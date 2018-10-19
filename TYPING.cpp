4ba493c2f457261bff3aa4e97c7b8125a4ccc1ca
reshu131 <reshu1310@gmail.com>
2018-10-19 22:59:49 +0530

    Typing.c++ constrant added

--- TYPING.cpp
+++ TYPING.cpp
@@ -10,11 +10,19 @@ int main(){
 		cin>>n;
 		
 		string a[n];
-		long i,j;
+		long i,j,r=0;
 		float t=0;
 		for(i=0;i<n;i++){
 			
 			cin>>a[i];
+			for(j=0;j<i;j++){
+				if(a[i]==a[j]){
+					r=1;
+				}
+			}
+			if(r==0){
+				
+			
 	t=t+0.2;
 		int l =	a[i].length();
 		char c[l+1];
@@ -29,7 +37,30 @@ int main(){
 			t = t + 0.4;
 		}
 		
-		}}
+		}
+		
+		}
+		
+		else{
+			
+		t=t+0.1;
+		int l =	a[i].length();
+		char c[l+1];
+		strcpy(c,a[i].c_str());
+		for ( j=0 ; j<l-1;j++){
+		
+		if(((c[j]=='d'|| c[j]=='f' )&&(c[j+1]=='j'||c[j+1]=='k'))||((c[j]=='j'|| c[j]=='k' )&&(c[j+1]=='d'||c[j+1]=='f')))
+		{
+			t = t+0.1;
+		}
+		else if (((c[j]=='d'||c[j]=='f')&&(c[j+1]=='d'||c[j+1]=='f'))||((c[j]=='j'||c[j]=='k')&&(c[j+1]=='j'||c[j+1]=='k'))){
+			t = t + 0.2;
+		}
+		
+		}r=0;
+			
+		}
+		}
 cout<<t<<endl;
 	}
 }
